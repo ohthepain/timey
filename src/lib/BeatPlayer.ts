4487f4190508fbcbfba1f02f9b108a5913800b04
Paul Wilkinson <cremoni@gmail.com>
2025-04-11 13:49:22 -0700

    cleanup

--- src/lib/BeatPlayer.ts
+++ src/lib/BeatPlayer.ts
@@ -1,8 +1,6 @@
 import { EventEmitter } from 'events';
 import TempoService from '~/lib/MidiSync/TempoService';
-import { useScoreStore } from '~/state/ScoreStore';
-import { NoteEntry, MakeStaveNotes, ConvertNoteToMidiNote } from '~/lib/ParseBeat';
-import { useMidiService } from './MidiService';
+import { NoteEntry, ConvertNoteToMidiNote } from '~/lib/ParseBeat';
 import { useMidiSettingsStore } from '~/state/MidiSettingsStore';
 import { midiService } from '~/lib/MidiService';
 
@@ -21,19 +19,6 @@ class BeatPlayer extends EventEmitter {
     TempoService.eventsEmitter.addListener('stop', this.handleStop.bind(this));
     TempoService.eventsEmitter.addListener('MIDI pulse', this.handleMidiPulse.bind(this));
   }
-
-  // public loadBeat(beatName: string) {
-  //   const beatString = useScoreStore.getState().getBeat(beatName);
-  //   if (!beatString) {
-  //     throw new Error(`Beat "${beatName}" not found in the store.`);
-  //   }
-
-  //   const { noteEntries } = MakeStaveNotes(beatString);
-  //   this.allNotes = noteEntries;
-  //   this.noteIndex = 0;
-  //   this.numLoops = 0;
-  //   this.nextNoteStartTime = this.allNotes[0].getStartTimeMsec(TempoService.bpm);
-  // }
 
   private handlePlay() {
     console.log('BeatPlayer: handlePlay');
