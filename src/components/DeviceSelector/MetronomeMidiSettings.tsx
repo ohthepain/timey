5a0d6cc9a5d0c3ec259c6352062b1a1b33004034
Paul Wilkinson <cremoni@gmail.com>
2025-05-05 12:57:43 +0200

    metronome midi settings show drum note name for each value

--- src/components/DeviceSelector/MetronomeMidiSettings.tsx
+++ src/components/DeviceSelector/MetronomeMidiSettings.tsx
@@ -3,6 +3,57 @@ import { useMidiSettingsStore } from '~/state/MidiSettingsStore';
 import { midiService } from '~/lib/MidiService';
 import { GoUnmute, GoLock, GoUnlock } from 'react-icons/go';
 
+// General MIDI drum note numbers and names
+const DRUM_NOTES = [
+  { number: 35, name: 'Acoustic Bass Drum' },
+  { number: 36, name: 'Bass Drum 1' },
+  { number: 37, name: 'Side Stick' },
+  { number: 38, name: 'Acoustic Snare' },
+  { number: 39, name: 'Hand Clap' },
+  { number: 40, name: 'Electric Snare' },
+  { number: 41, name: 'Low Floor Tom' },
+  { number: 42, name: 'Closed Hi-Hat' },
+  { number: 43, name: 'High Floor Tom' },
+  { number: 44, name: 'Pedal Hi-Hat' },
+  { number: 45, name: 'Low Tom' },
+  { number: 46, name: 'Open Hi-Hat' },
+  { number: 47, name: 'Low-Mid Tom' },
+  { number: 48, name: 'Hi-Mid Tom' },
+  { number: 49, name: 'Crash Cymbal 1' },
+  { number: 50, name: 'High Tom' },
+  { number: 51, name: 'Ride Cymbal 1' },
+  { number: 52, name: 'Chinese Cymbal' },
+  { number: 53, name: 'Ride Bell' },
+  { number: 54, name: 'Tambourine' },
+  { number: 55, name: 'Splash Cymbal' },
+  { number: 56, name: 'Cowbell' },
+  { number: 57, name: 'Crash Cymbal 2' },
+  { number: 58, name: 'Vibraslap' },
+  { number: 59, name: 'Ride Cymbal 2' },
+  { number: 60, name: 'Hi Bongo' },
+  { number: 61, name: 'Low Bongo' },
+  { number: 62, name: 'Mute Hi Conga' },
+  { number: 63, name: 'Open Hi Conga' },
+  { number: 64, name: 'Low Conga' },
+  { number: 65, name: 'High Timbale' },
+  { number: 66, name: 'Low Timbale' },
+  { number: 67, name: 'High Agogo' },
+  { number: 68, name: 'Low Agogo' },
+  { number: 69, name: 'Cabasa' },
+  { number: 70, name: 'Maracas' },
+  { number: 71, name: 'Short Whistle' },
+  { number: 72, name: 'Long Whistle' },
+  { number: 73, name: 'Short Guiro' },
+  { number: 74, name: 'Long Guiro' },
+  { number: 75, name: 'Claves' },
+  { number: 76, name: 'Hi Wood Block' },
+  { number: 77, name: 'Low Wood Block' },
+  { number: 78, name: 'Mute Cuica' },
+  { number: 79, name: 'Open Cuica' },
+  { number: 80, name: 'Mute Triangle' },
+  { number: 81, name: 'Open Triangle' },
+];
+
 export default function MetronomeMidiSettings() {
   const {
     metronomeNoteNumber,
@@ -45,16 +96,21 @@ export default function MetronomeMidiSettings() {
             <GoUnmute size={16} />
           </span>
         </div>
-        <input
+        <select
           className="col-span-1"
-          type="number"
           value={metronomeNoteNumber}
           onChange={(e) => {
             const value = parseInt(e.target.value);
             setMetronomeNoteNumber(value);
             midiService.playNote(value, metronomeVelocity);
           }}
-        />
+        >
+          {DRUM_NOTES.map((note) => (
+            <option key={note.number} value={note.number}>
+              {note.number} – {note.name}
+            </option>
+          ))}
+        </select>
 
         {/* Downbeat row */}
         <button
@@ -80,9 +136,8 @@ export default function MetronomeMidiSettings() {
             <GoUnmute size={16} />
           </span>
         </div>
-        <input
+        <select
           className="col-span-1"
-          type="number"
           value={metronomeDownbeatNoteNumber}
           disabled={isDownbeatLocked}
           onChange={(e) => {
@@ -90,7 +145,13 @@ export default function MetronomeMidiSettings() {
             setMetronomeDownbeatNoteNumber(value);
             midiService.playNote(value, metronomeVelocity);
           }}
-        />
+        >
+          {DRUM_NOTES.map((note) => (
+            <option key={note.number} value={note.number}>
+              {note.number} – {note.name}
+            </option>
+          ))}
+        </select>
 
         {/* Upbeat row */}
         <button
@@ -116,9 +177,8 @@ export default function MetronomeMidiSettings() {
             <GoUnmute size={16} />
           </span>
         </div>
-        <input
+        <select
           className="col-span-1"
-          type="number"
           value={metronomeUpbeatNoteNumber}
           disabled={isUpbeatLocked}
           onChange={(e) => {
@@ -126,7 +186,13 @@ export default function MetronomeMidiSettings() {
             setMetronomeUpbeatNoteNumber(value);
             midiService.playNote(value, metronomeVelocity);
           }}
-        />
+        >
+          {DRUM_NOTES.map((note) => (
+            <option key={note.number} value={note.number}>
+              {note.number} – {note.name}
+            </option>
+          ))}
+        </select>
 
         {/* Velocity row */}
         <div />
